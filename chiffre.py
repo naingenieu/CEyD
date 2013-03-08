0b6d131f546eeb4042cad663880b0042dcc2f3e1
naingenieu <nain.genieu@gmail.com>
2013-03-08 19:27:38 +0100

    fix implicit limitation of 62 keys

--- chiffre.py
+++ chiffre.py
@@ -8,10 +8,9 @@
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://www.wtfpl.net/ for more details.
 
-import polynome, sys
+import polynome, sys, random
 from base64 import  b64encode
 from string import ascii_letters, digits
-from random import choice
 
 def genPoly(nombre, degre):
     pol = [nombre]
@@ -37,6 +36,6 @@ def chiffre(chaine, degre, nb_cle):
     pol = genPoly(string2Int(chaine), degre)
     keys = genKeys(pol, genListe(nb_cle))
     randpool = ascii_letters + digits
-    uid = "".join([choice(randpool) for k in range(degre+1)])
+    uid = "".join([random.choice(randpool) for k in range(degre+1)])
     return [b64encode(polynome.combine(uid,y)+":"+str(x)+":"+str(y)) for x,y in keys]
 
